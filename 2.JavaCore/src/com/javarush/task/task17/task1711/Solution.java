d9267af2e47f4ba582736de4790dd7657acf2e1b
Eugene Kortov <somemail@g>
2017-03-09 22:40:40 +0400

    Parsing args works, but logic of list operations is old and doesn't work

--- 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
@@ -20,8 +20,9 @@ public class Solution {
     }
 
     public static void main(String[] args) throws Exception {
-        doIt(args);
+        parseArgs(args);
     }
+
     public static void doIt(String[] args) throws Exception {
         switch (args[0]) {
             case "-c": {
@@ -41,6 +42,40 @@ public class Solution {
             }
         }
     }
+
+    public static String[][] parseArgs(String[] args) throws Exception {
+        String[][] newArgs = null;
+        switch (args[0]) {
+            case "-c": {
+                newArgs = parseArray(args, 3);
+                for (int i = 0; i < newArgs.length; i++) {
+                    createPerson(newArgs[i]);
+                }
+            }
+            case "-u": {
+                updatePerson(args);
+            }
+            case "-d": {
+                deletePerson(args);
+            }
+            case "-i": {
+                infoAboutPerson(args);
+            }
+        }
+        return newArgs;
+    }
+
+    private static String[][] parseArray(String[] args, int step) {
+        String[] shiftedArgs = new String[args.length - 1];
+        System.arraycopy(args, 1, shiftedArgs, 0, shiftedArgs.length);
+
+        String[][] newArgs = new String[shiftedArgs.length / step][step];
+        for (int i = 0, j = 0; i < shiftedArgs.length; i += step, j++) {
+                System.arraycopy(shiftedArgs,i,newArgs[j],0,step);
+        }
+        return newArgs;
+    }
+
     public static void createPerson(String[] args) throws Exception {
         Person person;
         if ("ж".equals(args[2])) {
@@ -51,25 +86,27 @@ public class Solution {
         allPeople.add(person);
         System.out.println(allPeople.size() - 1);
     }
+
     public static void updatePerson(String[] args) throws Exception {
         Person temp = allPeople.get(Integer.valueOf(args[1]));
-        if ("ж".equals(args[3])) {
-            temp.setName(args[2]);
+        if ("ж".equals(args[2])) {
+            temp.setName(args[1]);
             temp.setSex(Sex.FEMALE);
-            temp.setBirthDay(format.parse(args[4]));
-        }
-        else {
-            temp.setName(args[2]);
+            temp.setBirthDay(format.parse(args[3]));
+        } else {
+            temp.setName(args[1]);
             temp.setSex(Sex.MALE);
-            temp.setBirthDay(format.parse(args[4]));
+            temp.setBirthDay(format.parse(args[3]));
         }
     }
+
     public static void deletePerson(String[] args) {
         Person person = allPeople.get(Integer.valueOf(args[1]));
         person.setBirthDay(null);
         person.setSex(null);
         person.setName(null);
     }
+
     public static void infoAboutPerson(String[] args) throws Exception {
         Person person = allPeople.get(Integer.valueOf(args[1]));
         String result = person.getName();
