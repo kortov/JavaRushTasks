80b448fea76bdf86ff048c0d5d1e933f23c9749a
Eugene Kortov <kortovea@gmail.com>
2017-11-19 21:56:14 +0400

    task1923 is done

--- 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -4,7 +4,27 @@ package com.javarush.task.task19.task1923;
 Слова с цифрами
 */
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(args[0]));
+        BufferedWriter out = new BufferedWriter(new FileWriter(args[1]));
+
+        while (in.ready()) {
+            String[] readerWord = in.readLine().split(" ");
+            for (int i = 0; i < readerWord.length; i++) {
+                if (readerWord[i].matches(".*\\d.*")) {
+                    out.write(readerWord[i] + " ");
+                }
+            }
+        }
+        in.close();
+        out.close();
     }
 }
+
